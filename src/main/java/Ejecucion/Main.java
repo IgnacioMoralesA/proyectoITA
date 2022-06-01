f4dc6d61b2f2b5ca282bbeda44db11b23f311c69
Gustavo <g.calderon05@ufromail.cl>
2022-05-31 22:54:02 -0400

    Ordenamiento codigo y ediciones varias

---
+++ src/main/java/Ejecucion/Main.java
@@ -0,0 +1,9 @@
+package Ejecucion;
+
+import GUIs.VentanaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana= new VentanaPrincipal();
+    }
+}
